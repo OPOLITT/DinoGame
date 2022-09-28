266be0c52838750464357219527058f3f88e13a0
OPOLITT <93400206+OPOLITT@users.noreply.github.com>
2022-09-28 15:57:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+const dino = document.getElementById('dino');
+const cactus = document.getElementById('cactus');
+
+document.addEventListener('keydown', function () {
+    jump();
+})
+
+let jump = () => {
+    if (dino.classList != 'jump') {
+        dino.classList.add('jump')
+    }
+    setTimeout(() => {
+        dino.classList.remove('jump')
+    }, 300);
+
+}
+
+let isAlive = setInterval(() => {
+    let dinoStyle = parseInt(window.getComputedStyle(dino).getPropertyValue('top'))
+    let cactusStyle = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'))
+
+    if (dinoStyle > 125 && cactusStyle > -5 && cactusStyle < 45) {
+        alert('GAME OVER!!!!!!')
+    }
+}, 10)
